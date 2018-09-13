1abc8d552a0ef4390bd544b1e3a8799e03eeae7b
Lachlan Evenson <lachie83@users.noreply.github.com>
2018-09-13 15:09:09 -0700

    adding assets (#3)

---
+++ rps-demo-prometheus/public/assets/secondary-carousel.js
@@ -0,0 +1,9 @@
+$('#carousel0').owlCarousel({
+  items: 6,
+  autoPlay: 3000,
+  navigation: true,
+  navigationText: ['<i class="fa fa-chevron-left fa-5x"></i>',
+    '<i class="fa fa-chevron-right fa-5x"></i>'
+  ],
+  pagination: true
+});
