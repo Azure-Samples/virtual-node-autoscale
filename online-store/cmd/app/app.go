f87ff111efab62853165c69beaeb2e2774af162f
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-09-19 22:48:41 -0600

    App insight (#14)
    * WIP
    
    * Updated to use open census
    
    * Update deps
    
    * Update open-census issues
    
    * Fixing open census vendor
    
    * Add Local Forwarder Code/Dockerfile
    
    * Adding flag to toggle app insight
    
    * removed stray vendor
    
    * Adding Live Metrics Forwarder
    
    * Formatting fix

--- online-store/cmd/app/app.go
+++ online-store/cmd/app/app.go
@@ -6,20 +6,17 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 
 	"golang.org/x/time/rate"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-)
 
-// type metrics struct {
-// 	currentCount int
-// 	lastCount    int
-// 	rps          float64
-// 	lastTime     time.Time
-// }
+	"contrib.go.opencensus.io/exporter/ocagent"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/plugin/ochttp/propagation/tracecontext"
+	"go.opencensus.io/trace"
+)
 
 var (
 	requestDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
@@ -38,48 +35,60 @@ func instrumentHandler(
 ) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			now := time.Now()
 			t := prometheus.NewTimer(requestDurationsHistogram)
-			handler.ServeHTTP(w, r)
 			defer t.ObserveDuration()
-			diff := time.Since(now)
-			log.Printf("Finished request : %v", diff.Seconds())
+			handler.ServeHTTP(w, r)
 		},
 	)
 }
 
 func main() {
-	sleepSecondsStr := os.Getenv("SLEEP_SECONDS")
-	sleepSeconds, err := strconv.Atoi(sleepSecondsStr)
-	if err != nil {
-		log.Fatalf("bad value for sleep seconds: %s", sleepSecondsStr)
-	}
-
 	rpsLimitStr := os.Getenv("RPS_THRESHOLD")
 	rpsLimit, err := strconv.ParseFloat(rpsLimitStr, 64)
 	if err != nil {
 		log.Fatalf("bad value for rps limit: %s", rpsLimitStr)
 	}
 
-	// ctr := &metrics{
-	// 	lastTime: time.Now(),
-	// }
 	throttledHandler := throttler(
-		//	ctr,
 		rpsLimit,
-		sleepSeconds,
 		http.FileServer(http.Dir("/app/content")),
 	)
 	http.Handle("/metrics", promhttp.Handler())
 	http.Handle("/", instrumentHandler(throttledHandler))
-	//	go rpsTelemetryCalculator(ctr)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	appInsightEnabledStr := os.Getenv("APP_INSIGHT_ENABLED")
+	var handler http.Handler
+	if appInsightEnabledStr == "true" {
+		serviceName := os.Getenv("SERVICE_NAME")
+		if len(serviceName) == 0 {
+			serviceName = "go-app"
+		}
+		log.Printf("new ocagent named %s", serviceName)
+		exporter, err := ocagent.NewExporter(
+			ocagent.WithInsecure(),
+			ocagent.WithServiceName(serviceName),
+		)
+		if err != nil {
+			log.Fatal("Failed to create the agent exporter: %v", err)
+		}
+
+		trace.RegisterExporter(exporter)
+		// Always trace for this demo. In a production application, you should
+		// configure this to a trace.ProbabilitySampler set at the desired
+		// probability.
+		trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+		handler = &ochttp.Handler{
+			Propagation:      &tracecontext.HTTPFormat{},
+			IsPublicEndpoint: true,
+		}
+
+	}
+	log.Fatal(http.ListenAndServe(":8080", handler))
+
 }
 
 func throttler(
-	//ctr *metrics,
 	limit float64,
-	sleepSeconds int,
 	handler http.Handler,
 ) http.Handler {
 	limiter := rate.NewLimiter(rate.Limit(limit), 10)
